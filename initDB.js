20ba13f787e6248bf989d6ce46bf4f717c65297c
Pierre Karpov <push.default>
2016-11-08 00:13:50 -0800

    asdaasasdasdasdasd

--- initDB.js
+++ initDB.js
@@ -10,16 +10,21 @@
   variable matches its value in app.js  Otherwise, you'll have
   initialized the wrong database.
 */
-
 var mongoose = require('mongoose');
 var models   = require('./models');
 
 // Connect to the Mongo database, whether locally or on Heroku
 // MAKE SURE TO CHANGE THE NAME FROM 'lab7' TO ... IN OTHER PROJECTS
 var local_database_name = 'lab7';
-var local_database_uri  = 'mongodb://localhost/' + local_database_name
-var database_uri = process.env.MONGOLAB_URI || local_database_uri
-mongoose.connect(database_uri);
+var local_database_uri  = 'mongodb://localhost/' + local_database_name;
+var database_uri = /*'mongodb://heroku_mz4j8b60:ihn6kgm71pdhmgnjvlus5koa45@ds147797.mlab.com:47797/heroku_mz4j8b60';*/process.env.MONGODB_URI || process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || local_database_uri
+mongoose.connect(database_uri, function (err, res) {
+  if (err) {
+    console.log ('ERROR connecting to: ' + database_uri + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + database_uri);
+  }
+});
 
 
 // Do the initialization here
@@ -51,11 +56,10 @@ function onceClear(err) {
       console.log(to_save_count + ' left to save');
       if(to_save_count <= 0) {
         console.log('DONE');
-        // The script won't terminate until the 
+        // The script won't terminate until the
         // connection to the database is closed
         mongoose.connection.close()
       }
     });
   }
 }
-
